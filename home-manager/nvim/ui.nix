8c2a8d97eff03cbba660d32e28d393c9c8b694fb
EineFrau <tyyirowi@zohomail.eu>
2026-03-15 16:46:31 +0100

    change: themes

--- home-manager/nvim/ui.nix
+++ home-manager/nvim/ui.nix
@@ -2,8 +2,8 @@
   programs.nvf.settings.vim = {
     theme = {
       enable = true;
-      name = "dracula";
-      style = "dark";
+      name = "github";
+      style = "dark_high_contrast";
     };
   };
 }
